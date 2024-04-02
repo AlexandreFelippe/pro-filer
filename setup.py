85a75cd30502b031106443636b4953295224792b
AlexandreFelippe <alemfelippe@gmail.com>
2024-04-02 14:37:05 -0300

    Atualiza versão do pip e remove setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="pro_filer",
-    description="Projeto Pro Filer",
-    install_requires=["pypubsub==4.0.3"],
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"]
-)
