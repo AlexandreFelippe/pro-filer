0bb518a45d582d45462b17246455a5461d870134
AlexandreFelippe <alemfelippe@gmail.com>
2024-04-02 15:03:05 -0300

    Resolve requisito 01

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -1,11 +1,16 @@
-"""Arquivo que estudantes devem editar"""
+def is_match(search_term, file_name, case_sensitive=True):
+    if not case_sensitive:
+        search_term = search_term.lower()
+        file_name = file_name.lower()
+
+    return search_term in file_name
 
 
 def show_deepest_file(context):
-    if not context["all_files"]:
+    if "all_files" not in context or not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(context["all_files"], key=lambda x: x.count('/'))
         print(f"Deepest file: {deepest_file}")
 
 
@@ -18,11 +23,7 @@ def find_file_by_name(context, search_term, case_sensitive=True):
     for path in context["all_files"]:
         file_name = path.split("/")[-1]
 
-        if not case_sensitive:
-            file_name.lower()
-            search_term.lower()
-
-        if search_term in file_name:
+        if is_match(search_term, file_name, case_sensitive):
             found_files.append(path)
 
     return found_files
